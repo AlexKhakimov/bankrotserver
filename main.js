c8decbe4be8801989b833c739939bc07ad10a78b
Алексей Хакимов <hakimowf@gmail.com>
2025-05-29 16:24:52 +0300

    4

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 import express from "express";
 const { Pool } = pkg;
 import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+// Middleware
+app.use(cors());
+app.use(express.json());
+// Подключение к PostgreSQL
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
